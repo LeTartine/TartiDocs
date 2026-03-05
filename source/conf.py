5be2b459fb0dc0c74c13cfaa5667acd4192ae72e
Adrien Souyris <adrien.souyris@proton.ch>
2026-03-05 09:20:54 +0100

    version change

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'TartiDoc'
 copyright = '2026, Tartine'
 author = 'Tartine'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
